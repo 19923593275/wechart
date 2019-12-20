0c921d99fd6eefae6baf8daaf45d68b291f46629
19923593275 <1143022649@qq.com>
2019-12-20 20:07:59 +0800

    修改定时执行任务

--- store/src/main/java/com/zxx/wechart/store/StoreApplication.java
+++ store/src/main/java/com/zxx/wechart/store/StoreApplication.java
@@ -1,13 +1,37 @@
 package com.zxx.wechart.store;
 
+import com.zxx.wechart.store.config.TokenUtil;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class, args);
+	}
+
+	@Component
+	@Order(1)
+	public class accessTokenStart implements CommandLineRunner {
+		@Override
+		public void run(String... args) throws Exception {
+			System.out.println("执行了第一个任务");
+			TokenUtil.startTask();
+		}
+	}
+
+	@Component
+	@Order(2)
+	public class createMenuStart implements  CommandLineRunner {
+
+		@Override
+		public void run(String... args) throws Exception {
+			System.out.println("第二个任务开始执行======================");
+		}
 	}
 
 }
