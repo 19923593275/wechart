5855f8a9f07035f9b25dbb1c13b5dab4e8d50d1b
19923593275 <1143022649@qq.com>
2020-01-15 17:34:25 +0800

    新增mybatisconfig配置

---
+++ store/src/main/java/com/zxx/wechart/store/domain/user/User.java
@@ -0,0 +1,9 @@
+package com.zxx.wechart.store.domain.user;
+
+/**
+ * @Author ： 周星星
+ * @Date ： 2020/1/15 13:47
+ * @DES : @TODO
+ */
+public class User {
+}
---
+++ store/src/main/java/com/zxx/wechart/store/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.zxx.wechart.store.mapper;
+
+/**
+ * @Author ： 周星星
+ * @Date ： 2020/1/15 13:58
+ * @DES : @TODO
+ */
+public interface UserMapper {
+}
